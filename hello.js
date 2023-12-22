8cc705eeafc448e38586e3c7fe43e8764bcd79e2
wakejalker <32205542+wakejalker@users.noreply.github.com>
2023-12-21 22:08:16 -0600

    Completed first program

--- hello.js
+++ hello.js
@@ -4,8 +4,16 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
-
+console.log(23);
+console.log("will this print after?","maybe this one will print on the same line"); 
+console.log("let's see");
+console.log("&quot Is this a quote? &quot");
+/* i thought css/html might work but i'm probably remembering it incorrectly */
+console.log("Google says I could use &quot;a variety of ways to print double quotation marks&quot;");
+/* hmmmmmmmmmmm */
+console.log("\"Okay apparently I can also use backslashes?\"");
+/* WAHOO */
+console.log("I'm not sure why this feels different from last time but it does!");
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
