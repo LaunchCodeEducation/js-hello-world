2946bfc0b2bfe53489a93b5df78b245002ed9f0e
Msinfiniti50 <153966011+Msinfiniti50@users.noreply.github.com>
2023-12-22 22:39:57 -0600

    Update My First code.js
    My homework

--- My First code.js
+++ My First code.js
@@ -4,9 +4,20 @@
 
 
 
-console.log("Hello Worldd!");
-console.log('Hello, my name is Janellq');
+console.log('Hello, World!');
 
+
+console.log('Hello, my name is Tunisia');
+//console.log"Say my name, say my name";This is a syntax error, the parentheses are needed to make the code work  properly.//
+
+
+
+//consolw.log('Hayyy')The parentheses are needed to make a string but not numbers to work properly.//
+console.log(5+5+5)
+console.log(4);console.log(5+5);
+console.log('Monday,Tuesday,Wednesday,Thursday,Friday');
+console.log('This has been a great day' + ' I am learning so much');
+console.log("Quoth the Raven \"Nevermore\"");
 
 
 
